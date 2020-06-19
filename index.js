20c3a0a3a9cf4cb30d366bc4cba085bb48a8a338
Raúl Armendáriz Reyes <raul.armendariz@nox-consulting.com>
2020-06-19 09:22:59 -0500

    export ready

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const csv = require("csv-parser");
 const fs = require("fs");
 const moment = require("moment");
+const createCsvWriter = require("csv-writer").createObjectCsvWriter;
 
 let data = [];
 fs.createReadStream("data1.csv")
@@ -42,5 +43,60 @@ fs.createReadStream("data1.csv")
       sets.push(set);
       pointer = index;
     }
-    console.log(sets);
+    //console.log(sets);
+
+    let finalData = [];
+    sets.forEach((s) => {
+      let formatDate = moment(s[0].date);
+      const element = {
+        date: formatDate.format("DD/MM/YYYY HH:mm"),
+        first: null,
+        second: null,
+        third: null,
+        sistolica: null,
+        diastolica: null,
+        pulso: null,
+      };
+
+      let results = [];
+      let sistolica = 0;
+      let diastolica = 0;
+      let pulso = 0;
+      s.forEach((lecture) => {
+        let systolic = lecture.sys;
+        let diastolic = lecture.dia;
+        let pulse = lecture.pulse;
+        let format = `${systolic}/${diastolic} - ${pulse}`;
+        sistolica += parseInt(systolic);
+        diastolica += parseInt(diastolic);
+        pulso += parseInt(pulse);
+        results.push(format);
+      });
+      if (results[0]) element.first = results[0];
+      if (results[1]) element.second = results[1];
+      if (results[2]) element.third = results[2];
+      if (results[3]) element.third = results[3];
+      element.sistolica = sistolica / s.length;
+      element.diastolica = diastolica / s.length;
+      element.pulso = pulso / s.length;
+      finalData.push(element);
+    });
+
+    //console.log(finalData);
+    const csvWriter = createCsvWriter({
+      path: "./final.csv",
+      header: [
+        { id: "date", title: "FECHA" },
+        { id: "first", title: "Primera" },
+        { id: "second", title: "Segunda" },
+        { id: "third", title: "Tercera" },
+        { id: "sistolica", title: "Tercera" },
+        { id: "diastolica", title: "Tercera" },
+        { id: "pulso", title: "Tercera" },
+      ],
+    });
+
+    csvWriter.writeRecords(finalData).then(() => {
+      console.log("creado");
+    });
   });
